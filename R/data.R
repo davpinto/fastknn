80920584d294a60c4dcc88370567c5ba026ce349
davpinto <davidengaut@gmail.com>
2016-11-20 02:49:43 -0200

    Transform project into a R package

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Chess Dataset
+#'
+#' Bidimensional binary dataset to be used in toy examples.
+#'
+#' @format A data frame with 2000 rows and 2 variables:
+#' \describe{
+#'   \item{x1}{random generated}
+#'   \item{x2}{random generated}
+#' }
+"chess"
