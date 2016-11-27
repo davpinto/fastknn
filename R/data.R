6757e08a654cdc62a9bbf553f4c4801587228de6
davpinto <davidengaut@gmail.com>
2016-11-27 16:00:54 -0200

    Update documentation of the datasets

--- R/data.R
+++ R/data.R
@@ -1,46 +1,46 @@
 #' Chess Dataset
 #'
-#' Bidimensional binary dataset to be used in toy examples.
+#' Bidimensional binary dataset to be used as a toy example.
 #'
 #' @format A data.frame with 2000 rows and 3 variables:
 #' \describe{
-#'   \item{x1}{random generated}
-#'   \item{x2}{random generated}
-#'   \item{target}{class labels}
+#'   \item{x1}{randomly generated numeric feature}
+#'   \item{x2}{randomly generated numeric feature}
+#'   \item{target}{factor array with class labels}
 #' }
 "chess"
 
 #' Spirals Dataset
 #'
-#' Bidimensional binary dataset to be used in toy examples.
+#' Bidimensional binary dataset to be used as a toy example.
 #'
 #' @format A data.frame with 2000 rows and 3 variables:
 #' \describe{
-#'   \item{x1}{random generated}
-#'   \item{x2}{random generated}
-#'   \item{target}{class labels}
+#'   \item{x1}{randomly generated numeric feature}
+#'   \item{x2}{randomly generated numeric feature}
+#'   \item{target}{factor array with class labels}
 #' }
 "spirals"
 
 #' Multi Spirals Dataset
 #'
-#' Bidimensional multiclass dataset to be used in toy examples.
+#' Bidimensional multiclass dataset to be used as a toy example.
 #'
 #' @format A data.frame with 2000 rows and 3 variables:
 #' \describe{
-#'   \item{x1}{random generated}
-#'   \item{x2}{random generated}
-#'   \item{target}{class labels}
+#'   \item{x1}{randomly generated numeric feature}
+#'   \item{x2}{randomly generated numeric feature}
+#'   \item{target}{factor array with class labels in 4 levels}
 #' }
 "multi_spirals"
 
 #' Covertype Data
 #'
-#' Sample of size 50k from the multiclass dataset Covertype from the UCI repository.
+#' Sample of size 50k from the multiclass dataset Covertype hosted in the UCI repository.
 #'
 #' @format A data.frame with 50000 rows and 55 variables:
 #' \describe{
 #'   \item{V1...V54}{binary features}
-#'   \item{Target}{class labels}
+#'   \item{Target}{character array with class labels in 7 categories}
 #' }
 "covertype"
